47d2c397ae51fd5e074eaae773fa0e3272dc940a
spoortinaik99 <56391234+spoortinaik99@users.noreply.github.com>
2021-06-30 11:23:45 +0530

    changes made.statements added

--- quiz.c
+++ quiz.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 main(){
-    printf("QUIZ GAME\n");
-    printf("--------");
+    printf("\t\t\t     QUIZ GAME\n");
+    printf("\n\t\t\t   WELCOME ");
+     printf("\n\t\t\t      to ");
+     printf("\n\t\t\t   THE GAME ");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t   BECOME A MILLIONAIRE!!!!!!!!!!!    ") ;
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+
 }
