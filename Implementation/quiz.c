451d069c1189ce0424141b068debab768fda9ce1
spoortinaik99 <56391234+spoortinaik99@users.noreply.github.com>
2021-07-03 15:09:35 +0530

    changed

--- Implementation/quiz.c
+++ Implementation/quiz.c
@@ -6,48 +6,33 @@
 #include<string.h>
 
 int main(){
-    
-    mainhome:
     system("cls");
     printf("\t\t\t     QUIZ GAME\n");
-    printf("\n\t\t\t   WELCOME ");
-     printf("\n\t\t\t      to ");
-     printf("\n\t\t\t   THE GAME ");
-     printf("\n\t\t________________________________________");
-     printf("\n\t\t________________________________________");
-     printf("\n\t\t   BECOME A MILLIONAIRE!!!!!!!!!!!    ") ;
-     printf("\n\t\t________________________________________");
-     printf("\n\t\t________________________________________");
      printf("\n\t\t > Press S to start the game");
      printf("\n\t\t > Press V to view the highest score  ");
      printf("\n\t\t > Press R to reset score");
      printf("\n\t\t > press H for help            ");
      printf("\n\t\t > press Q to quit             ");
      printf("\n\t\t________________________________________\n\n");
-     char  choice=toupper(getch());
+    char  choice=toupper(getch());
     if(choice=='V')
 	 {
          system("cls");
          show_record();
-         system("cls");
-         goto mainhome;
+
 	 }
      else if(choice=='H')
 	 {
          system("cls");
          help();
-         getch();
-         system("cls");
-	     goto mainhome;
+         
 	 }
      else if(choice=='R')
 	 {
          system("cls");
          reset_score();
-	     getch();
-	     goto mainhome;
-      }
-	 
+	     
+     }
      else if (choice=='Q')
 	 {
          exit(1);
