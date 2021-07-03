61f05a717a596546d3477243ddfae179a41201ee
spoortinaik99 <56391234+spoortinaik99@users.noreply.github.com>
2021-07-03 13:31:53 +0530

    changed

--- Implementation/quiz.c
+++ Implementation/quiz.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+void show_record();
+void reset_score();
+void help();
+void edit_score(float,char[]);
 main(){
     
     mainhome:
@@ -20,5 +28,39 @@ main(){
      printf("\n\t\t________________________________________\n\n");
    
 
-
+     char  choice=toupper(getch());
+    if(choice=='V')
+	 {
+         system("cls");
+         show_record();
+         system("cls");
+         goto mainhome;
+	 }
+     else if(choice=='H')
+	 {
+         system("cls");
+         help();
+         getch();
+         system("cls");
+	     goto mainhome;
+	 }
+     else if(choice=='R')
+	 {
+         system("cls");
+         reset_score();
+	     getch();
+	     goto mainhome;
+      }
+	 
+     else if (choice=='Q')
+	 {
+         exit(1);
+     }    
+     else if(choice=='S'){
+         printf("\n\n\n\n\n\n\n\n\n\n\t\t\tResister your name:");
+     }
+     else {
+         printf("wrong choice\n");
+         goto mainhome;
+     }
 }
