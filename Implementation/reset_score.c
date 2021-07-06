3a8c25e3e885e868ee37ddffa3344ee035e6cb6e
spoortinaik99 <56391234+spoortinaik99@users.noreply.github.com>
2021-07-06 17:48:22 +0530

    reset score updated

--- Implementation/reset_score.c
+++ Implementation/reset_score.c
@@ -1,4 +1,14 @@
 #include "fun.h"
+#include<stdio.h>
 void reset_score(){
+    system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r+");
+	fscanf(f,"%s%f",&nm,&sc);
+	sc=0;
+	fprintf(f,"%s,%.2f",nm,sc);
+    fclose(f);
 
 }
